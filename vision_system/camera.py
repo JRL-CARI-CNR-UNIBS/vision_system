2de4c14da79cde150f1788899747cdbb4ad4f6b9
Samuele Sandrini <samuele.sandrini@polito.it>
2024-08-23 10:46:02 +0200

    Docs

--- vision_system/camera.py
+++ vision_system/camera.py
@@ -24,6 +24,7 @@ import rclpy.wait_for_message
 from sensor_msgs.msg import CameraInfo, Image
 from vision_system.post_processing import PostProcessing
 import numpy as np
+from typing import Optional, Tuple
 
 DEFAULT_COLOR_IMAGE_TOPIC = '/camera/color/image_raw'
 DEFAULT_DEPTH_IMAGE_TOPIC = '/camera/depth/image_raw'
@@ -33,15 +34,15 @@ DEFAULT_DEPTH_FRAME_ENCODING = '32FC1'
 
 class Camera(Node):
     """
-    RealSense class for Subscribe interesting topic.
+    Camera class for acquiring images (color and depth) from a RGBD-camera.
     """
 
     def __init__(self, 
-                 color_image_topic = DEFAULT_COLOR_IMAGE_TOPIC, 
-                 depth_image_topic = DEFAULT_DEPTH_IMAGE_TOPIC, 
-                 camera_info_topic = DEFAULT_CAMERA_INFO_TOPIC, 
-                 frames_approx_sync = DEFAULT_FRAMES_APPROX_SYNC,
-                 depth_frame_encoding = DEFAULT_DEPTH_FRAME_ENCODING,
+                 color_image_topic: str = DEFAULT_COLOR_IMAGE_TOPIC, 
+                 depth_image_topic: str = DEFAULT_DEPTH_IMAGE_TOPIC, 
+                 camera_info_topic: str = DEFAULT_CAMERA_INFO_TOPIC, 
+                 frames_approx_sync: bool = DEFAULT_FRAMES_APPROX_SYNC,
+                 depth_frame_encoding: str = DEFAULT_DEPTH_FRAME_ENCODING,
                  ):
 
         super().__init__('camera_node')
@@ -62,13 +63,13 @@ class Camera(Node):
 
         self._cv_bridge = CvBridge()
 
-        self.color_frame = None
-        self.depth_frame = None
-        self.distance_frame = None
+        self.color_frame: Optional[np.ndarray] = None
+        self.depth_frame: Optional[np.ndarray] = None
+        self.distance_frame: Optional[np.ndarray] = None
         self.intrinsics = None
-        self.camera_info = None
+        self.camera_info: Optional[CameraInfo] = None
 
-        self.post_processing: PostProcessing = None
+        self.post_processing: Optional[PostProcessing] = None
 
         self.image_sub = message_filters.Subscriber(
             self, Image, color_image_topic)
@@ -83,7 +84,7 @@ class Camera(Node):
             (self.image_sub, self.depth_sub), 10)
         
     
-    def _convert_frames(self, color_frame, depth_frame):
+    def _convert_frames(self, color_frame: Image, depth_frame: Image) -> bool:
       try:
         self.color_frame = self._cv_bridge.imgmsg_to_cv2(color_frame, desired_encoding='passthrough')
         self.depth_frame = self._cv_bridge.imgmsg_to_cv2(depth_frame, desired_encoding='passthrough')
@@ -93,7 +94,12 @@ class Camera(Node):
         return False
       return True
       
-    def retrieve_camera_info(self):
+    def retrieve_camera_info(self) -> bool:
+      """
+      Retrieves the camera info waiting for the topic and stores it in the class.
+
+      :return: True if camera info is successfully retrieved, False otherwise.
+      """
       self.get_logger().info('Waiting for camera info...')
       retrieved, self.camera_info = rclpy.wait_for_message.wait_for_message(
             CameraInfo, self, self.camera_info_topic, time_to_wait=3.0
@@ -104,7 +110,12 @@ class Camera(Node):
       self.get_logger().info('Camera info retrieved.')
       return True
         
-    def acquire_color_frame_once(self):
+    def acquire_color_frame_once(self) -> Optional[np.ndarray]:
+      """
+      Acquires a single color frame waiting for the topic.
+
+      :return: The acquired color frame as a cv2 image as NumPy array, or None if acquisition fails.
+      """
       retrieved, color_frame = rclpy.wait_for_message.wait_for_message(
             Image, self, self.color_image_topic, time_to_wait=3.0
         )
@@ -120,14 +131,19 @@ class Camera(Node):
       
       return self.color_frame.copy()
 
-    def acquire_frames_once(self):
+    def acquire_frames_once(self) -> Tuple[Optional[np.ndarray], Optional[np.ndarray]]:
+      """
+      Acquires a single pair of color and depth frames.
+
+      :return: A tuple of (color_frame, depth_frame) as NumPy arrays, or (None, None) if acquisition fails.
+      """
       self._reset_frames()
       image_sub = message_filters.Subscriber(
         self, Image, self.color_image_topic)
       depth_sub = message_filters.Subscriber(
         self, Image, self.depth_image_topic)
       
-      def acquire_frames_once_callback(color_frame, depth_frame):
+      def acquire_frames_once_callback(color_frame: Image, depth_frame: Image) -> None:
         self._convert_frames(color_frame, depth_frame)
         self.destroy_subscription(image_sub)
         self.destroy_subscription(depth_sub)
@@ -147,14 +163,27 @@ class Camera(Node):
       else:
         return None, None
     
-    def process_once(self):
+    def process_once(self) -> Optional[np.ndarray]:
+      """
+      Processes a single pair of color and depth frames using the provided post-processing function.
+
+      :return: Processed frames or None if acquisition or processing fails.
+      """
       color_frame, distance_frame = self.acquire_frames_once()
       if color_frame is None or distance_frame is None:
         return None
       if self.post_processing is not None:
         self.post_processing.process_frames(color_frame, distance_frame)
            
-    def set_processing_function(self, package_name, module_name, class_name):
+    def set_processing_function(self, package_name: str, module_name: str, class_name: str) -> None:
+      """
+      Dynamically loads and sets the post-processing function.
+
+      :param package_name: The name of the package containing the post-processing module.
+      :param module_name: The name of the module containing the post-processing class.
+      :param class_name: The name of the post-processing class.
+      :raises: Exception if loading fails or if the class is not an instance of PostProcessing.
+      """
       try:
           module = importlib.import_module(f'{package_name}.{module_name}')
           
@@ -166,7 +195,10 @@ class Camera(Node):
           self.get_logger().error(f'Error importing proccessing function: {e}')
           raise e
 
-    def start_acquire(self):
+    def start_acquire(self) -> None:
+      """
+      Starts the acquisition of synchronized color and depth frames, with post-processing.
+      """
       if self.camera_info is None:
         self.retrieve_camera_info()
       self._synchronizer.registerCallback(self._process)
@@ -189,7 +221,10 @@ class Camera(Node):
 
       return self.color_frame.copy(), self.distance_frame.copy()
 
-    def start_acquire_only_color(self):
+    def start_acquire_only_color(self) -> None:
+      """
+      Starts the acquisition of color frames only, with post-processing (is necessary to spin the node).
+      """
       self.color_sub= self.create_subscription(
             Image,
             self.color_image_topic,
@@ -197,26 +232,51 @@ class Camera(Node):
             10
         )
 
-    def get_frames(self):
+    def get_frames(self) -> Optional[Tuple[np.ndarray, np.ndarray]]:
+      """
+      Retrieves the most recently acquired color and depth frames.
+
+      :return: A tuple of (color_frame, depth_frame) as NumPy arrays, or None if frames are not available.
+      """
       if self.color_frame is not None and self.distance_frame is not None:
         return self.color_frame.copy(), self.distance_frame.copy()
   
-    def get_color_frame(self) -> np.ndarray:
+    def get_color_frame(self) -> Optional[np.ndarray]:
+      """
+      Retrieves the most recently acquired color frame.
+
+      :return: The color frame as a NumPy array, or None if no frame is available.
+      """
       if self.color_frame is not None:
         return self.color_frame.copy()
       return None
     
-    def get_distance_frame(self) -> np.ndarray:
+    def get_distance_frame(self) -> Optional[np.ndarray]:
+      """
+      Retrieves the most recently acquired depth frame.
+
+      :return: The depth frame as a NumPy array, or None if no frame is available.
+      """
       if self.distance_frame is not None:
         return self.distance_frame.copy()
       return None
     
-    def get_camera_info(self) -> CameraInfo:
+    def get_camera_info(self) -> Optional[CameraInfo]:
+      """
+      Retrieves the camera info.
+
+      :return: The camera info as a CameraInfo object, or None if it has not been retrieved.
+      """
       if self.camera_info is not None:
         return self.camera_info.copy()
       return None
     
-    def get_frame_id(self):
+    def get_frame_id(self) -> Optional[str]:
+      """
+      Retrieves the frame ID from the camera info.
+
+      :return: The frame ID as a string, or None if camera info is not available.
+      """
       if self.camera_info is not None:
         return self.camera_info.header.frame_id
       return None
