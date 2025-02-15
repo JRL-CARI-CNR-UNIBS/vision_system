69c2e2414de9c3daf376dda98a4394336134aef4
Samuele Sandrini <samuele.sandrini@polito.it>
2025-02-15 11:19:01 +0100

    ci

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ package_name = 'vision_system'
 setup(
     name=package_name,
     version='1.0.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
